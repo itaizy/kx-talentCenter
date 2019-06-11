19a58ade31165bf4374aa42a113fb2b4def69bd5
itaizy <itaizy@163.com>
2019-06-11 14:11:05 +0800

    fetchNews

--- backend/serverapi/views.py
+++ backend/serverapi/views.py
@@ -14,23 +14,6 @@ def index(request):#定义一个函数，第一个参数必须是request
     print('lalala')
     return HttpResponse("Hello, world. Hello，python.")#返回HttpResonse对象，最终将这行字符显示在页面上
 
-# def qqscorefree(request):
-#     print(request.GET['score'], request.GET['local_type_id'], request.GET['province_id'])
-#     res = QQscore10Model.objects.get(score=request.GET['score'], local_type_id=request.GET['local_type_id'], province_id=request.GET['province_id'])
-#     # res = QQscore10Model.objects.get(score=520, local_type_id=1)
-#     print(res.data)
-    
-#     return JsonResponse({
-#         'score':res.score, 
-#         'local_type_id':res.local_type_id, 
-#         'counts': res.counts, 
-#         'srank':res.srank, 
-#         'data':res.data,
-#         'nuniv':res.nuniv,
-#         'nsp':res.nsp,
-#         'total':res.total
-#     }, safe=False)
 def fetchNews(request):
-    print('sdasfda')
-    res = Kexiedata.objects.order_by('-date')[0:10]
-    print(res)
+    res = Kexiedata.objects.order_by('-date')[0:10].values_list()
+    return JsonResponse({'data': list(res)}, safe=False)
